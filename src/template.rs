77955093281e2cfd019426a38005e31ae5fb15d1
aanesn <anesen.niklas@gmail.com>
2025-08-02 18:01:13 +0200

    add gitingore to templates + small fixes

--- src/template.rs
+++ src/template.rs
@@ -8,30 +8,28 @@ struct Templates;
 pub fn scaffold(loc: &PathBuf, name: &str, fw: &Framework) -> anyhow::Result<()> {
     std::fs::create_dir_all(loc)?;
 
-    let template = fw.to_string();
+    let prefix = format!("{}/", fw);
 
-    for path in Templates::iter() {
-        if !path.starts_with(&template) {
-            continue;
-        }
-
-        let rel = path.strip_prefix(&format!("{}/", template)).unwrap();
-        let dest = loc.join(rel);
+    for file in Templates::iter().filter(|f| f.starts_with(&prefix)) {
+        let path = {
+            let rel = file.strip_prefix(&prefix).unwrap();
+            loc.join(rel)
+        };
 
         // don't overwrite
-        if dest.exists() {
+        if path.exists() {
             continue;
         }
 
-        if let Some(parent) = dest.parent() {
-            std::fs::create_dir_all(parent)?;
-        }
-
-        let file = Templates::get(&path).unwrap();
+        let file = Templates::get(&file).unwrap();
         let content = std::str::from_utf8(file.data.as_ref())?;
         let processed = content.replace("{% name %}", name);
 
-        std::fs::write(dest, processed)?;
+        if let Some(parent) = path.parent() {
+            std::fs::create_dir_all(parent)?;
+        }
+
+        std::fs::write(path, processed)?;
     }
 
     Ok(())
