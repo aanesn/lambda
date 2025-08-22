39ba8703e483e32ad3701f4c47032c00d85256d4
aanesn <anesen.niklas@gmail.com>
2025-08-21 20:48:33 -0400

    button

--- apps/landing/src/lib/components/ui/Header.svelte
+++ apps/landing/src/lib/components/ui/Header.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Logomark from "../icons/Logomark.svelte"
+	import Button from "./Button.svelte"
 </script>
 
 <header class="flex h-20 items-center justify-between">
 	<Logomark />
+	<Button class="duration-300">Start now</Button>
 </header>
