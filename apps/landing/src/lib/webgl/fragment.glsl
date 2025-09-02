b6a76a007d400f26ec0e4276c79c9e210dfe9397
aanesn <anesen.niklas@gmail.com>
2025-09-01 21:28:35 -0400

    add colors back

--- apps/landing/src/lib/webgl/fragment.glsl
+++ apps/landing/src/lib/webgl/fragment.glsl
@@ -1,5 +1,8 @@
 precision mediump float;
+varying vec3 v_position;
 
 void main() {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    float gradient = (v_position.y + 1.0) * 0.5;
+    vec3 color = mix(vec3(0.8, 0.7, 0.9), vec3(0.9, 0.85, 0.95), gradient);
+    gl_FragColor = vec4(color, 1.0);
 }
--- apps/landing/src/lib/webgl/vertex.glsl
+++ apps/landing/src/lib/webgl/vertex.glsl
@@ -1,6 +1,8 @@
 attribute vec4 a_position;
 uniform mat4 u_matrix;
+varying vec3 v_position;
 
 void main() {
     gl_Position = u_matrix * a_position;
+    v_position = a_position.xyz;
 }
