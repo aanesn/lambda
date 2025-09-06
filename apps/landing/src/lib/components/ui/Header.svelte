4da25792bf4c82830144eb161625fcd3e5e7769c
aanesn <anesen.niklas@gmail.com>
2025-09-06 14:50:00 -0400

    header fix

--- apps/landing/src/lib/components/ui/Header.svelte
+++ apps/landing/src/lib/components/ui/Header.svelte
@@ -4,15 +4,10 @@
 	import Link from "./Link.svelte"
 </script>
 
-<header
-	class="sticky top-0 z-50 flex h-16 items-center justify-between bg-black/80 backdrop-blur-lg"
->
+<header class="flex h-[72px] items-center justify-between">
 	<Logomark />
 	<div class="flex items-center gap-x-6">
 		<Button class="duration-300">Start now</Button>
 		<Link class="duration-300" href="/">Docs</Link>
 	</div>
-	<hr
-		class="absolute bottom-0 h-px w-full border-0 bg-gradient-to-r from-transparent via-white/10 to-transparent"
-	/>
 </header>
