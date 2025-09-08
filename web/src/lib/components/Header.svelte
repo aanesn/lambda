2b3eb7758aba0edc964320fd709a2040df7c1d7b
aanesn <anesen.niklas@gmail.com>
2025-09-08 06:06:17 -0400

    menu icon

--- web/src/lib/components/Header.svelte
+++ web/src/lib/components/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Logomark from "$lib/assets/logomark.svg"
+	import Menu from "$lib/assets/menu.svg"
 	import Button from "./Button.svelte"
 	import Link from "./Link.svelte"
 </script>
@@ -8,6 +9,7 @@
 	<a href="/">
 		<img src={Logomark} alt="logomark" />
 	</a>
+	<img src={Menu} alt="menu" class="flex lg:hidden" />
 	<nav class="absolute left-1/2 hidden -translate-x-1/2 items-center gap-x-6 lg:flex">
 		<Link href="/">Products</Link>
 		<Link href="/">Solutions</Link>
