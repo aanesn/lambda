b7ed89de891d4627e92724408c8fc2796d50eb1f
aanesn <anesen.niklas@gmail.com>
2025-09-12 19:33:11 -0400

    center on mobile

--- web/src/routes/(landing)/+page.svelte
+++ web/src/routes/(landing)/+page.svelte
@@ -11,15 +11,15 @@
 <section class="pt-24 lg:pt-44">
 	<div class="flex flex-col gap-y-6">
 		<h1
-			class="max-w-xl font-manrope text-5xl font-medium -tracking-[1%] text-balance md:text-6xl lg:text-7xl"
+			class="text-center font-manrope text-5xl font-medium -tracking-[1%] text-balance md:text-6xl lg:max-w-xl lg:text-left lg:text-7xl"
 		>
 			AWS Lambda without the AWS
 		</h1>
-		<h2 class="w-11/12 max-w-lg text-white/80 lg:text-lg">
+		<h2 class="text-center text-balance text-white/80 lg:max-w-lg lg:text-left lg:text-lg">
 			Universal Lambda deployment tool integrated with your AWS account, for full control and
 			no markup.
 		</h2>
-		<div class="flex items-center gap-x-4">
+		<div class="flex items-center justify-center gap-x-4 lg:justify-start">
 			<Button class="duration-300">Start now</Button>
 			<Link href="/" class="duration-300">Book a demo</Link>
 		</div>
