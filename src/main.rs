6b616ada04183ed2c15c8865f3a5a19bd8df3ce9
aanesn <anesen.niklas@gmail.com>
2025-09-13 14:38:33 -0400

    oauth client

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 use axum::{Router, routing::get};
+use oauth2::{AuthUrl, ClientId, ClientSecret, RedirectUrl, TokenUrl, basic::BasicClient};
 use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
 
-    let db_url = std::env::var("DATABASE_URL").expect("db url is not set");
+    let database_url = std::env::var("DATABASE_URL").expect("db url is not set");
+    let github_client_id = std::env::var("GITHUB_CLIENT_ID").expect("gh client id is not set");
+    let github_client_secret =
+        std::env::var("GITHUB_CLIENT_SECRET").expect("gh client secret is not set");
 
     let db = PgPoolOptions::new()
         .max_connections(5)
-        .connect(&db_url)
+        .connect(&database_url)
         .await
         .expect("failed to connect to db");
+
+    let oauth = BasicClient::new(ClientId::new(github_client_id))
+        .set_client_secret(ClientSecret::new(github_client_secret))
+        .set_auth_uri(AuthUrl::new("https://github.com/login/oauth/authorize".to_string()).unwrap())
+        .set_token_uri(
+            TokenUrl::new("https://github.com/login/oauth/access_token".to_string()).unwrap(),
+        )
+        .set_redirect_uri(
+            RedirectUrl::new("http://127.0.0.1:8080/auth/callback".to_string()).unwrap(),
+        );
 
     let app = Router::new().route("/", get(|| async { "hello world!" }));
 
