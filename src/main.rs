8e5691360edbe88aecc6c80982508bc922377d8f
aanesn <anesen.niklas@gmail.com>
2025-09-12 22:49:38 -0400

    db init

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 use axum::{Router, routing::get};
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().ok();
+
+    let db_url = std::env::var("DATABASE_URL").expect("db url is not set");
+
+    let db = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await
+        .expect("failed to connect to db");
+
     let app = Router::new().route("/", get(|| async { "hello world!" }));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
