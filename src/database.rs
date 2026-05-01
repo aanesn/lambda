5aa18126e637dd5183e3905d384a24ebf6eb69db
aanesn <anesen.niklas@gmail.com>
2026-05-02 01:11:49 +0200

    db init

---
+++ src/database.rs
@@ -0,0 +1,23 @@
+use crate::error::AppError;
+use axum::{
+    extract::{FromRef, FromRequestParts},
+    http::request::Parts,
+};
+
+pub type ConnectionPool = bb8::Pool<redis::Client>;
+
+pub struct DatabaseConnection(bb8::PooledConnection<'static, redis::Client>);
+
+impl<S> FromRequestParts<S> for DatabaseConnection
+where
+    ConnectionPool: FromRef<S>,
+    S: Send + Sync,
+{
+    type Rejection = AppError;
+
+    async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let pool = ConnectionPool::from_ref(state);
+        let conn = pool.get_owned().await?;
+        Ok(Self(conn))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
+use crate::database::ConnectionPool;
 use axum::{
     Router,
     routing::{get, post},
 };
 
+mod database;
 mod error;
 mod webhook;
 
+#[derive(Clone)]
+struct Ctx {
+    pool: ConnectionPool,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
+
+    let pool = bb8::Pool::builder()
+        .build(redis::Client::open(std::env::var("REDIS_URL")?)?)
+        .await?;
+
+    let ctx = Ctx { pool };
+
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/webhook", post(webhook::post));
+        .route("/webhook", post(webhook::post))
+        .with_state(ctx);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await?;
     println!("listening on http://{}", listener.local_addr()?);
