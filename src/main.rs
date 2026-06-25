04f0024009eb684e1320faceae586fdf33c36fcf
aanesn <anesen.niklas@gmail.com>
2026-06-25 09:16:29 +0200

    axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await?;
+    println!("listening on http://{}", listener.local_addr()?);
+    Ok(axum::serve(listener, app).await?)
 }
