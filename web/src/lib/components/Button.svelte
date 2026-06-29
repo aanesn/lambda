25ab1bbd6ab7e13d0e9b3306571416cdf67a3511
aanesn <anesen.niklas@gmail.com>
2026-06-29 23:44:42 +0200

    button

---
+++ web/src/lib/components/Button.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { cva, cx } from "$lib/utils"
+	import type { VariantProps } from "cva"
+	import type { HTMLAnchorAttributes, HTMLButtonAttributes } from "svelte/elements"
+
+	const button = cva({
+		base: "inline-flex shrink-0 items-center justify-center rounded-xl text-sm whitespace-nowrap transition-all select-none [&_svg]:pointer-events-none [&_svg]:shrink-0",
+		variants: {
+			intent: {
+				primary: "bg-white text-black hover:bg-white/80"
+			},
+			size: {
+				md: "h-8 px-3"
+			}
+		}
+	})
+
+	let {
+		class: className,
+		children,
+		intent = "primary",
+		size = "md",
+		href = undefined,
+		type = "button",
+		...others
+	}: HTMLButtonAttributes &
+		HTMLAnchorAttributes & {
+			intent?: VariantProps<typeof button>["intent"]
+			size?: VariantProps<typeof button>["size"]
+		} = $props()
+</script>
+
+{#if href}
+	<a {href} class={cx(button({ intent, size }), className)} {...others}>
+		{@render children?.()}
+	</a>
+{:else}
+	<button class={cx(button({ intent, size }), className)} {type} {...others}>
+		{@render children?.()}
+	</button>
+{/if}
--- web/src/routes/(landing)/components/Header.svelte
+++ web/src/routes/(landing)/components/Header.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import Logomark from "$lib/assets/logomark.svg?raw"
+	import Button from "$lib/components/Button.svelte"
 </script>
 
 <header class="flex items-center justify-between h-16">
 	<a href="/">
 		{@html Logomark}
 	</a>
+	<Button class="duration-300">start now</Button>
 </header>
