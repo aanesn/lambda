aef073b4ce6313d1b29fe0af6729d70cc5fb83bf
aanesn <anesen.niklas@gmail.com>
2026-06-29 06:28:21 +0200

    header

---
+++ web/src/routes/(landing)/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Header from "./components/Header.svelte"
+
+	let { children } = $props()
+</script>
+
+<div class="mx-auto container px-6 xl:max-w-7xl">
+	<Header />
+	{@render children()}
+</div>
---
+++ web/src/routes/(landing)/components/Header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Logomark from "$lib/assets/logomark.svg?raw"
+</script>
+
+<header class="flex items-center justify-between h-16">
+	<a href="/">
+		{@html Logomark}
+	</a>
+</header>
